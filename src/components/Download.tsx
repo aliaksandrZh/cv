402e0d4208eea048ee628b9a4a1ca2c04ab33c07
aliaksandrZh <108681764+aliaksandrZh@users.noreply.github.com>
2024-06-16 22:44:43 +0300

    update pdf

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -3,7 +3,7 @@ import { HoverAnimationItemWrapper } from "./ui/HoverAnimationItemWrapper";
 export const DownloadCV = () => {
   return (
     <HoverAnimationItemWrapper>
-      <a href="/Aliaksandr.Zhebit.pdf" download>
+      <a href="/Aliaksandr.Zhebit.pdf" download="Aliaksandr.Zhebit.Web.Dev.pdf">
         <button className="bg-title px-12 py-3 text-white shadow-lg transition-all hover:scale-105 active:scale-90 sm:px-20 sm:py-5">
           Download CV.pdf
         </button>
