d7767d841ab078559f7a449242f011fd38671056
aliaksandrZh <108681764+aliaksandrZh@users.noreply.github.com>
2024-06-17 11:04:21 +0300

    restore certificates

--- src/components/Exp.tsx
+++ src/components/Exp.tsx
@@ -1,6 +1,5 @@
 import { AboutMe } from "./AboutMe";
 import { Certificates } from "./Certificates";
-import { DownloadCV } from "./Download";
 import { Education } from "./Education";
 import { Experience } from "./Experience";
 import { Expertise } from "./Expertise";
@@ -22,19 +21,7 @@ export const Exp = () => {
         <Expertise />
       </section>
       <section className="border-title p-5 md:border-r-2">
-        <DownloadCV
-          href="/Aliaksandr.Zhebit.pdf"
-          download="Aliaksandr.Zhebit.Web.Dev.pdf"
-          className="sticky top-1/4 text-center lg:text-left"
-          text="Download CV.pdf"
-        />
-        <DownloadCV
-          href="/Aliaksandr.Zhebit.docx"
-          download="Aliaksandr.Zhebit.Web.Dev.docx"
-          text="Download CV.docx"
-          className="sticky top-1/3 mt-5 text-center lg:text-left"
-        />
-        {/* <Certificates /> */}
+        <Certificates />
       </section>
       <section className="border-title p-5 md:border-r-2">
         <Education />
