90e6a9146f48758cfa5fe6780455322b5b9849e2
aliaksandrZh <108681764+aliaksandrZh@users.noreply.github.com>
2026-05-26 12:11:46 +0300

    removing typing animation

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,10 @@ import "./css/theme.css";
 import "./css/styles.css";
 
 import { initTheme } from "./theme";
-import { initTypewriter } from "./typewriter";
 import { initDownload } from "./download";
 import { initLang } from "./lang";
 
-const expertise = [
-  "JavaScript",
-  "TypeScript",
-  "C#",
-  "Node.js",
-  "Angular",
-  "React",
-  "RxJs",
-  "Tailwind CSS",
-  "ASP.NET Core",
-  "Docker",
-  "SSMS",
-  "SQL",
-  "SignalR",
-  "RabbitMQ",
-  "Azure",
-  "git",
-];
-
 initTheme();
-initTypewriter("#typed-text", expertise);
 initDownload();
 initLang();
 
